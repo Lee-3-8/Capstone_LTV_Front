62e55cfb360044450200ac32047b945f80f287dd
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-05-28 11:48:11 +0900

    [Component] add statistic number animation

---
+++ src/component/Mystatistic.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Statistic } from 'antd';
+import CountUp from 'react-countup';
+
+const Mystatistic = ({ title, value, suffix = '' }) => {
+  const formatter = v => (
+    <CountUp
+      style={{
+        fontSize: '24px',
+      }}
+      start={0}
+      end={v}
+      duration={2}
+      separator=","
+      suffix={suffix}
+    />
+  );
+  return <Statistic title={title} value={value} formatter={formatter} />;
+};
+
+export default Mystatistic;

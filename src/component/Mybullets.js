3e9cbf6f4f50f9386e7c5e29d3f096057b7237ee
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-06-03 20:21:44 +0900

    [마지막]

--- src/component/Mybullets.js
+++ src/component/Mybullets.js
@@ -2,22 +2,14 @@ import React from 'react';
 import { Progress } from '@ant-design/charts';
 
 const Mybullets = ({ title, value }) => {
-  // const formatter = v => {
-  //   const config = {
-  //     height: '10%',
-  //     width: '80%',
-  //     autoFit: false,
-  //     percent: v,
-  //     color: ['#5B8FF9', '#E8EDF3'],
-  //   };
-  //   return <Progress {...config} />;
-  // };
-  // return <Statistic title={title} value={value} formatter={formatter} />;
   const config = {
     percent: value,
     height: '100%',
     width: '100%',
     color: ['#5B8FF9', '#E8EDF3'],
+    tooltip: {
+      customContent: data => data,
+    },
   };
   return (
     <div>

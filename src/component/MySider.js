576f8071cc39882d2e6c123425fa05f9191f68b8
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-06-03 14:50:19 +0900

    [fix] bullet fix

--- src/component/MySider.js
+++ src/component/MySider.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Image, Layout, Menu } from 'antd';
-import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons';
+import { HomeOutlined, StarFilled, SettingFilled } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const { Sider } = Layout;
@@ -18,7 +18,7 @@ const MySider = () => {
       <Image width={200} src="../logo.png" preview={false} />
       <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
         <Menu.Item key="1">
-          <StarOutlined />
+          <HomeOutlined />
           <Link to="/main">Main</Link>
         </Menu.Item>
         <SubMenu
@@ -43,21 +43,13 @@ const MySider = () => {
             <Link to="/geo">Geo</Link>
           </Menu.Item>
           <Menu.Item key="6">
-            <Link to="/Hour">Hour</Link>
+            <Link to="/hour">Hour</Link>
           </Menu.Item>
         </SubMenu>
-        <SubMenu
-          key="sub2"
-          title={
-            <span>
-              <StarTwoTone />
-              <span>Others</span>
-            </span>
-          }
-        >
-          <Menu.Item key="7">Settings</Menu.Item>
-          <Menu.Item key="8">Dev-Info</Menu.Item>
-        </SubMenu>
+        <Menu.Item key="7">
+          <SettingFilled />
+          <Link to="/setting">Settings</Link>
+        </Menu.Item>
       </Menu>
     </Sider>
   );
